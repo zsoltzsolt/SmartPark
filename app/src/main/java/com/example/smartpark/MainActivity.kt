9325fbf50d87a889e7aab95844a63401eac6bea6
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2024-04-13 13:29:18 +0300

    Add webview element

--- app/src/main/java/com/example/smartpark/MainActivity.kt
+++ app/src/main/java/com/example/smartpark/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.smartpark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.example.smartpark.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        webViewSetup()
+    }
+
+    private fun webViewSetup() {
+        binding.wbWebView.webViewClient = WebViewClient()
+        binding.wbWebView.apply {
+            loadUrl("https://www.google.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if(binding.wbWebView.canGoBack())
+            binding.wbWebView.goBack()
+        else
+            super.onBackPressed()
     }
 }
