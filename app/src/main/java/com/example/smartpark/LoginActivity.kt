848a873bd5eaed3d0e43559e3e98599186e13e33
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2024-04-13 14:53:36 +0300

    Add login activity xml file

---
+++ app/src/main/java/com/example/smartpark/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.smartpark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
