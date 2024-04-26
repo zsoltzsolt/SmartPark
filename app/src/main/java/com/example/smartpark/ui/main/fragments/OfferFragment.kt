c9b70af43d6c2442d4542bf2633aa83d671cc35f
Diriczi Zsolt-Csaba <z.diriczi@yahoo.com>
2024-04-26 16:45:56 +0300

    Add border-color and lato font

--- app/src/main/java/com/example/smartpark/ui/main/fragments/OfferFragment.kt
+++ app/src/main/java/com/example/smartpark/ui/main/fragments/OfferFragment.kt
@@ -6,9 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.smartpark.R
+import com.example.smartpark.databinding.FragmentHomeBinding
+import com.example.smartpark.databinding.FragmentOfferBinding
 
 
 class OfferFragment : Fragment() {
+
+    private var _binding: FragmentOfferBinding? = null
+    private val binding get() = _binding!!
+
+    private var selectedButton: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +26,31 @@ class OfferFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_offer, container, false)
+        _binding = FragmentOfferBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val monthlyButton = binding.btnImgMonthly
+        val yearlyButton = binding.btnImgYearly
+
+        monthlyButton.setBackgroundResource(android.R.color.black)
+        yearlyButton.setBackgroundResource(android.R.color.black)
+
+        val buttons = listOf(monthlyButton, yearlyButton)
+
+        for (button in buttons) {
+            button.setOnClickListener {
+                if (selectedButton > -1) {
+                    buttons[selectedButton].setBackgroundResource(android.R.color.black)
+                }
+                selectedButton = buttons.indexOf(button)
+                it.setBackgroundResource(R.drawable.offer_button_background)
+            }
+        }
+
+    }
+
 }
